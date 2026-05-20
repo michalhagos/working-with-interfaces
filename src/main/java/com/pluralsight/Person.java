9c5057031933307a4725ee3b530eeb39b1b94c1e
michalhagos <mhayele3@gmail.com>
2026-05-19 21:38:02 -0500

    Add constructor to Person class with firstName lastName and age parameters

---
+++ src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class Person {
+
+    // the first name of the person
+    private String firstName;
+    // the last name of the person
+    private String lastName;
+    // the age of the person
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
+
+}
